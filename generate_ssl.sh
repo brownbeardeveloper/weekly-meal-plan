27b7ffb4fe419b20b8fe3dcc00bf65c0fe343ece
Björn Bergström <bjorn.bergstr@outlook.com>
2025-01-22 13:20:30 +0100

    add generate ssl script

---
+++ generate_ssl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Ensure script stops on errors
+set -e
+
+# Generate SSL certificate
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout nginx/certs/server.key \
+    -out nginx/certs/server.crt
+
+# Check if the command was successful
+if [ $? -eq 0 ]; then
+    echo "SSL certificate successfully generated in nginx/certs"
+else
+    echo "Failed to generate SSL certificate"
+    exit 1
+fi
